64326ef53be5b3401d96884483ab8dc31b2ac895
hbk0105 <adsfdas@naver.com>
2023-02-22 15:06:24 +0900

    commit

---
+++ vue-todo/src/store/modules/todoApp.js
@@ -0,0 +1,52 @@
+const storage = {
+    fetch(){
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+        return arr;
+    },
+}
+
+const state = {
+    headerText : 'TODO IT',
+    todoItems : storage.fetch(),
+}
+
+const getters = {
+    storeTodoItems(state){
+        return state.todoItems;
+    }
+}
+
+const mutations = {
+    addTodoItem(state , todoItem){
+        const obj = { completed: false, item: todoItem };
+        localStorage.setItem(todoItem, JSON.stringify(obj));
+        state.todoItems.push(obj);
+    },
+     removeOneItem(state , obj){
+        localStorage.removeItem(obj.todoItem.item);
+        state.todoItems.splice(obj.index, 1);
+    },  
+     toggleOneItem(state , obj){
+        state.todoItems[obj.index].completed = !state.todoItems[obj.index].completed;
+        localStorage.removeItem(obj.todoItem.item);
+        localStorage.setItem(obj.todoItem.item, JSON.stringify(obj.todoItem));
+    },
+    clearAll(state){
+        localStorage.clear();
+        state.todoItems = [];
+    }
+}
+
+export default{
+    state,
+    getters,
+    mutations,
+}
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,53 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+
+import todoApp from './modules/todoApp';
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch(){
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-                    // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    },
-   
-}
+
 
 export const store = new Vuex.Store({ 
-    //      
-    state : {
-        headerText : 'TODO IT',
-        todoItems : storage.fetch()
-    },
-    getters : {
-        storeTodoItems(state){
-            return state.todoItems;
-        }
-    },
-    mutations : {
-        addTodoItem(state , todoItem){
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-         removeOneItem(state , obj){
-            localStorage.removeItem(obj.todoItem.item);
-            state.todoItems.splice(obj.index, 1);
-        } , 
-        toggleOneItem(state , obj){
-            state.todoItems[obj.index].completed = !state.todoItems[obj.index].completed;
-            localStorage.removeItem(obj.todoItem.item);
-            localStorage.setItem(obj.todoItem.item, JSON.stringify(obj.todoItem));
-        },
-        clearAll(state){
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    modules : {
+        todoApp
+    }      
+    // state : {
+    //     headerText : 'TODO IT',
+    //     todoItems : storage.fetch()
+    // },
+    // getters,
+    // mutations,
 });
